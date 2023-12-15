d4a6c6f141a7f8e221d299f874b97f60c02d66d0
Jeff Ziegener <JZiegener@gmail.com>
2023-12-15 09:57:53 -0500

    add wait time to stop camera busy error

--- photoScript.sh
+++ photoScript.sh
@@ -4,10 +4,9 @@ filename="firstgrow-$currentDate.cr2"
 echo filename $filename
 gphoto2 --reset
 #take photo
-if gphoto2 --capture-image-and-download --camera="Canon EOS 1500D" --filename=$filename ;
+if gphoto2 --wait-event=2s --capture-image-and-download --camera="Canon EOS 1500D" --filename=$filename ;
 then
         echo "captured picture" >> /home/birch/photolog.txt
-#       gphoto2 --capture-image-and-download --filename=$filename
         mv $filename /mnt/pve/Media/RawPhotos/growTimelapse1/$filename
 else
         echo "capture failed" >> /home/birch/photolog.txt
